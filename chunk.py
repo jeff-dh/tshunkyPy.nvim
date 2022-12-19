4d8592d5d8490f11edb412877457fbc8dfa00f1c
jeff <1105041+jeff-dh@users.noreply.github.com>
2022-12-19 22:10:01 +0100

    improved chunk state capturing and restoring

--- chunk.py
+++ chunk.py
@@ -1,7 +1,23 @@
-import io
 import sys
 import logging
+import dill
+import types
 
+class StateWrapper(dict):
+    def __init__(self):
+        super().__init__()
+        self.data = None
+
+    def setData(self, data):
+        self.data = data
+
+    def __getitem__(self, key):
+        assert self.data
+        return self.data.__getitem__(key)
+
+    def __setitem__(self, key, value):
+        assert self.data
+        return self.data.__setitem__(key, value)
 
 class Chunk(object):
     def __init__(self, chash, objHash, codeObject, sourceChunk, lineno,
@@ -14,7 +30,13 @@ class Chunk(object):
         self.lineRange = range(lineno, end_lineno + 1)
         self.prevChunk = prevChunk
 
-        self.postState = None
+        # all chunks -- of the same "execution chain" / ChunkManager -- share
+        # the same stateWrapper. Only for the DummyInitialChunk
+        # (-> prevChunk is None) a new StateWrapper is created -- and shared
+        # with all other chunks
+        self.stateWrapper = prevChunk.stateWrapper if prevChunk \
+                                                   else StateWrapper()
+
         self.valid = False
         self.output = None
 
@@ -27,24 +49,51 @@ class Chunk(object):
 
         assert self.prevChunk
         assert self.prevChunk.valid
-        self.postState = dict(self.prevChunk.postState)
 
-        stdoutBuffer = io.StringIO()
-        prev_stdout, sys.stdout = sys.stdout, stdoutBuffer
-        self.postState['sys'] = sys
+        # store the sys.modules before we execute this chunk
+        beforeModules = set([m for m in sys.modules.keys()])
 
-        self.output = None
+        # derive namespace from prevChunk, based on a dill copy
+        self.namespace = dill.copy(self.prevChunk.namespace)
 
-        try:
-            exec(self.codeObject, self.postState)
-        except Exception as e:
-            self.output = repr(e) + stdoutBuffer.getvalue().strip()
-            self.valid = False
-        else:
-            self.output = stdoutBuffer.getvalue().strip()
-            self.valid = True
+        # copy function by reference! Otherwise their __globals__
+        # field gets invalid
+        for k, v in self.prevChunk.namespace.items():
+            if isinstance(v, types.FunctionType):
+                self.namespace[k] = v
 
-        sys.stdout = prev_stdout
+        # set our local namespace as "global namespace". This needs to be
+        # wrapped, because all function objects contain a reference to the
+        # global namespace (at chunk execution time! -> func.__globals__).
+        # But since we want it to run on this chunks globals, we need a
+        # wrapper to exchange the global namespace under the hood
+        self.stateWrapper.setData(self.namespace)
+
+        # and execute the chunk and capture stdout
+        with dill.temp.capture() as stdoutBuffer:
+            errorMsg = ''
+            try:
+                exec(self.codeObject, self.stateWrapper)
+            except Exception:
+                import traceback
+                errorMsg = traceback.format_exc()
+                self.valid = False
+            else:
+                self.valid = True
+
+            self.output = errorMsg + stdoutBuffer.getvalue().strip()
+
+        # unload modules that are not imported in the outside world
+        # (outside of the exec envinronment) this is necessary to
+        # make import xxxx work properly without reusing previously
+        # imported instances and their state
+        # buuut this does only work for modules that are not imported
+        # in the outside world (outside the exec environment).....
+        # I did not found a solution to save and restore the state of sys
+        # for example... :(
+        afterModules = set([m for m in sys.modules.keys()])
+        for m in (afterModules - beforeModules):
+            del sys.modules[m]
 
         return self.valid
 
@@ -54,6 +103,6 @@ class Chunk(object):
 class DummyInitialChunk(Chunk):
     def __init__(self, initialNamespace):
         super().__init__(0, 0, None, None, 0, 0, None)
-        self.postState = initialNamespace
+        self.namespace = initialNamespace
         self.valid = True
 
